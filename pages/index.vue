b0f121aa414f8dc57a4c72e2d657cc3f4c7cc5d8
idkaryadi <karyadi@alterra.id>
2021-07-19 00:30:09 +0700

    add simple example

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,9 @@
       sm8
       md6
     >
-      <v-card>
-        <v-card-text class="headline text-xs-center">Mengenal Huruf</v-card-text>
+      <v-card class="mb-2">
+        <v-card-text class="title text-xs-center">Aku Adalah Huruf</v-card-text>
+        <v-btn @click="refreshSound" class="mt-5" fab absolute small top right><v-icon>refresh</v-icon></v-btn>
         <v-card-text>
           <div class="text-xs-center display-4 font-weight-bold pb-3">{{data.huruf}}</div>
         </v-card-text>
@@ -24,7 +25,7 @@
             <v-icon>chevron_left</v-icon>
           </v-btn>
           <v-btn
-            @click="soundOn"
+            @click="klikHuruf"
             elevation="2"
             fab
             outlined
@@ -42,6 +43,11 @@
           </v-btn>
         </v-card-text>
       </v-card>
+      <!-- <div class="title text-xs-center pt-2 pb-1">Contoh Kata</div> -->
+      <v-btn class="coba" @click="klikKata">{{data.kata[0]}}</v-btn>
+      <v-btn class="coba" @click="klikKata">{{data.kata[1]}}</v-btn>
+      <v-btn class="coba" @click="klikKata">{{data.kata[2]}}</v-btn>
+      <v-btn class="coba" @click="klikKata">{{data.kata[3]}}</v-btn>
     </v-flex>
   </v-layout>
 </template>
@@ -59,30 +65,44 @@ export default {
     daftarAbjad: daftarAbjad,
   }),
   methods: {
-    soundOn() {
+    klikKata(e) {
+      this.soundOn(e.target.innerText)
+    },
+    klikHuruf() {
+      this.soundOn(this.data.huruf)
+    },
+    refreshSound() {
+      window.speechSynthesis.cancel();
+    },
+    soundOn(value) {
       const msg = new SpeechSynthesisUtterance();
       msg.volume = 1; // 0 to 1
-      msg.rate = 1; // 0.1 to 10
+      msg.rate = 0.9; // 0.1 to 10
       // msg.pitch = 1; //0 to 2
-      msg.text = this.data.huruf;
+      msg.text = value;
+      // msg.text = 'this.data.huruf'
       msg.lang = 'id';
       window.speechSynthesis.speak(msg);
     },
     handlePrev() {
       if (this.index - 1 > -1) {
-        console.log("newindex", this.index-1)
         this.index = this.index - 1
         this.data = abjad[daftarAbjad[Number(this.index)]]
       }
     },
     handleNext() {
       if (this.index + 1 < 26) {
-        console.log("newindex", this.index+1)
         this.index = this.index + 1;
-        console.log("abjad[daftarAbjad[Number(this.index+1)]]", abjad[daftarAbjad[Number(this.index)]])
         this.data = abjad[daftarAbjad[Number(this.index)]]
       }
     }
   }
 }
 </script>
+
+<style scoped>
+  .coba {
+    width: 100%;
+    margin-left: 0px;
+  }
+</style>
